f99483157daa334ab4b017de40830ac934dd3acb
Cornelius <cornelius@kyco.co.za>
2014-02-08 14:00:57 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,21 @@
+kyco-cp-theme
+=============
+
+An EE Control Panel theme. It focuses on a highly efficient navigation.
+
+How to install
+--------------
+
+Just copy and paste the files into the root of your EE installation.
+
+
+Configuration
+-------------
+
+You can manually replace the logo.png to give the login screen a more personal touch.
+
+
+Support
+-------
+
+For bugs or improvements please use the issues tab or email info@kyco.co.za.
---
+++ themes/cp_themes/kyco/js/ee_navigation.js
@@ -0,0 +1,30 @@
+/*!
+ * @author	Cornelius Weidmann
+ */
+
+$(document).ready(function() {
+	// Remove sidebar because it breaks layout in Chrome
+	$('#sideBar').remove();
+
+	// Unbind all events from default ee_navigation script
+	$('#navigationTabs').unbind();
+	$('#navigationTabs li').unbind();
+
+	// Attach new functionality
+	$('#navigationTabs > li.parent').hover(function() {
+		$(this).addClass('active');
+	}, function() {
+		var _this = $(this);
+		var listItemParent = _this.parents('ul#navigationTabs').find(_this.parents('li.parent'));
+
+		if (listItemParent.length !== -1) {
+			listItemParent.removeClass('active');
+		}
+
+		$('ul#navigationTabs > li.parent').each(function() {
+			if (_this.hasClass('active')) {
+				_this.removeClass('active');
+			}
+		});
+	});
+});
